06e5251f198ef09de80883f8bf932581c37ae887
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-05-28 13:39:53 -0400

    Adding hoverable lists.

---
+++ services.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".expandable-item").forEach(item => {
+    item.addEventListener("click", () => {
+      item.classList.toggle("open");
+    });
+  });
+});
